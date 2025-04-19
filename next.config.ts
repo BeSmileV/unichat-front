c4c61d4e886df914acbfdab8f671eb7f77831601
sergey_bondarenko <sergey_bondarenko@main.ru>
2025-04-19 19:59:50 +0300

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find((rule: any) =>
+            rule.test?.test?.(".svg")
+        );
+
+        if (fileLoaderRule) {
+            // Добавляем обработку SVG как URL при ?url
+            config.module.rules.push(
+                {
+                    ...fileLoaderRule,
+                    test: /\.svg$/i,
+                    resourceQuery: /url/, // import icon from './icon.svg?url'
+                },
+                {
+                    test: /\.svg$/i,
+                    issuer: fileLoaderRule.issuer,
+                    resourceQuery: {not: [/url/]}, // import Icon from './icon.svg'
+                    use: ["@svgr/webpack"],
+                }
+            );
+
+            // Исключаем SVG из оригинального file loader
+            fileLoaderRule.exclude = /\.svg$/i;
+        }
+
+        return config;
+    }
 };
 
 export default nextConfig;
