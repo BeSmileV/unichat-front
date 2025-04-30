d18f1a61f3fea80d7e5c9e4146f92e4936e5060e
Bondarenko <besmilev@gmail.com>
2025-04-30 08:56:39 +0300

    fix indicator ui problems, update for working version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,12 @@
 'use client'
 
 import {InputField} from "indicator-ui";
-import {IMask} from "react-imask";
 
 export default function Home() {
     return (
         <main>
             <InputField type={'test'}
-                        mask={'HH:mm'}
-                        blocks={{
-                            HH: {
-                                mask: IMask.MaskedRange,
-                                from: 0,
-                                to: 23,  // для часов 00-23
-                            },
-                            mm: {
-                                mask: IMask.MaskedRange,
-                                from: 0,
-                                to: 59,  // для минут 00-59
-                            },
-                            ss: {
-                                mask: IMask.MaskedRange,
-                                from: 0,
-                                to: 59,  // для секунд 00-59
-                            },
-                            SSS: {
-                                mask: IMask.MaskedRange,
-                                from: 0,
-                                to: 999,  // для миллисекунд 000-999
-                            },
-                        }}/>
+                        mask={'HH:mm'}/>
         </main>
     )
 }
